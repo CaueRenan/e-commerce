280567ea1dd01eeed8e619f7bbfefe73f246b6bd
cauer <caue.renanss@gmail.com>
2025-04-09 16:52:05 -0300

    Route configuration

---
+++ src/components/ProductCollaction/index.tsx
@@ -0,0 +1,7 @@
+export const ProductCollaction = () => {
+  return (
+    <>
+      <h1>ProductCollaction</h1>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Product } from './components/Product';
+import { ProductCollaction } from './components/ProductCollaction';
+import { ShoppingCart } from './components/ShoppingCart';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productCollaction" element={<ProductCollaction />} />
+        <Route path="/ShoppingCart" element={<ShoppingCart />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
